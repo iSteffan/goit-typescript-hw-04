aea9047ec78a5ce358c851136e6382f292b526d1
Stepan Skichko <st.skichko@gmail.com>
2023-10-06 15:36:50 +0300

    task 3 DONE

--- src/homework/3.tsx
+++ src/homework/3.tsx
@@ -1,13 +1,14 @@
-// import React, { useState } from 'react';
+// Ви створюєте компонент форми у React.Ви маєте поле введення, в якому ви хочете відстежити зміни.Для цього ви використовуєте обробник подій onChange.
+// Ваше завдання – правильно типізувати подію, яка передається у цю функцію.
 
-// export function FormComponent() {
-//   const [value, setValue] = useState("");
+import React, { useState, ChangeEvent } from 'react';
 
-//   const handleChange = (event) => {
-//     setValue(event.target.value);
-//   };
+export function FormComponent() {
+  const [value, setValue] = useState<string>('');
 
-//   return (
-//     <input type="text" value={value} onChange={handleChange} />
-//   );
-// }
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  return <input type="text" value={value} onChange={handleChange} />;
+}
